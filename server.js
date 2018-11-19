30230604c9f835b09bf971856af66f56c1701c03
Jim <jim_lubbers@me.com>
2018-11-19 23:17:53 +0100

    Added webserver for heroku deployment

---
+++ server.js
@@ -0,0 +1,24 @@
+/* jshint esversion: 6 */
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '/public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+
+
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
